9206d25dfd0a84b3e62d496141b325e2ad2b44e8
Arafat-Zaman <arafat.zaman.anik@gmail.com>
2021-01-11 00:29:43 +0600

    changes

---
+++ CSharpAllConcepts/_42CustomException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAllConcepts
+{
+    public class _42CustomException
+    {
+        public static void Main()
+        {
+            throw new UserAlreadyloggedin();
+        }
+    }
+    public class UserAlreadyloggedin : Exception
+    {
+        public UserAlreadyloggedin() : base()
+        {
+
+        }
+
+        public UserAlreadyloggedin(string message) : base(message)
+        {
+            
+        }
+
+        public UserAlreadyloggedin(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
---
+++ CSharpAllConcepts/_43Exceptionhandling.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAllConcepts
+{
+    public class _43Exceptionhandling
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.WriteLine("1st: ");
+                int FN = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("2nd: ");
+                int SN = Convert.ToInt32(Console.ReadLine());
+
+                int result = FN / SN;
+                Console.WriteLine("{0}",result);
+            }
+            catch(FormatException)
+            {
+
+            }
+            catch(OverflowException)
+            {
+
+            }
+            catch(DivideByZeroException)
+            {
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
