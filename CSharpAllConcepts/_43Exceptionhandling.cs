e9e1ab53bd02fa5201723b6aaf3b85babb04f44f
Arafat-Zaman <arafat.zaman.anik@gmail.com>
2021-01-11 23:55:26 +0600

    changes

--- CSharpAllConcepts/_43Exceptionhandling.cs
+++ CSharpAllConcepts/_43Exceptionhandling.cs
@@ -13,12 +13,23 @@ namespace CSharpAllConcepts
             try
             {
                 Console.WriteLine("1st: ");
-                int FN = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("2nd: ");
-                int SN = Convert.ToInt32(Console.ReadLine());
+                int Numerator;
+                bool isNumeratorconversionsuccessful = Int32.TryParse(Console.ReadLine(), out Numerator);
 
-                int result = FN / SN;
-                Console.WriteLine("{0}",result);
+                if(isNumeratorconversionsuccessful)
+                {
+                    int FN = Convert.ToInt32(Console.ReadLine());
+
+                    int denominator;
+                    bool isDenominatorsuccessful = Int32.TryParse(Console.ReadLine(), out denominator);
+
+                    Console.WriteLine("2nd: ");
+                    int SN = Convert.ToInt32(Console.ReadLine());
+
+                    int result = FN / SN;
+                    Console.WriteLine("{0}", result);
+                }
+               
             }
             catch(FormatException)
             {
