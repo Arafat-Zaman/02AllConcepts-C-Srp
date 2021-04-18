1fd2da9c16db0b60044a4d4e0a19413b6d1dfc0f
Arafat Zaman <arafat.zaman@nuarca.com>
2021-04-18 15:10:48 +0600

    aysnc await

---
+++ AsyncAwait/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace AsyncAwait
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCount = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCount
+            // 
+            this.lblCount.Location = new System.Drawing.Point(159, 53);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(75, 23);
+            this.lblCount.TabIndex = 0;
+            this.lblCount.Text = "ProcessFile";
+            this.lblCount.UseVisualStyleBackColor = true;
+            this.lblCount.Click += new System.EventHandler(this.lblCount_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button lblCount;
+    }
+}
+
